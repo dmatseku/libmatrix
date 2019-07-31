d1310d61886527a25bbd26907390b96cd06de297
dmatseku <kamikoto.sudzuki@gmail.com>
2019-07-31 23:56:35 +0300

    refactored code

--- vector/vector_normal.c
+++ vector/vector_normal.c
@@ -5,6 +5,8 @@ t_vector*	vector_normal(t_vector *const restrict vector)
 	const float		m = sqrtf(vector->x * vector->x +
 								vector->y * vector->y +
 								vector->z * vector->z);
-	return (vector_create(vector->x / m, vector->y / m,
-						  vector->z / m, vector->w));
+	return ((m ? vector_create(vector->x / m, vector->y / m,
+						  vector->z / m, vector->w) :
+				 vector_create(vector->x, vector->y,
+						   vector->z, vector->w)));
 }
